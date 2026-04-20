a9a9a7799954258192b6b54abb17dcca45c4fdf7
mvayк <mvayk@proton.me>
2026-04-21 05:25:53 +1000

    reproduce ability

--- build.ps1
+++ build.ps1
@@ -1,1 +1,1 @@
-python3 src/main.py
+.\.venv\Scripts\python.exe src/main.py
--- shell.ps1
+++ shell.ps1
@@ -1,2 +1,4 @@
 python -m venv .venv/
 . .\.venv\Scripts\Activate.ps1
+.\.venv\Scripts\python.exe -m pip install --upgrade pip
+.\.venv\Scripts\python.exe -m pip install moderngl pygame numpy
