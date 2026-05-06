274ae382993a2b379f338c995ddb1685699c3060
mvayк <mvayk@proton.me>
2026-05-06 19:05:33 +1000

    switching devices

--- src/engine/engine.py
+++ src/engine/engine.py
@@ -12,23 +12,25 @@ import utils.utils as utils
 # from PIL import Image;
 
 class Engine:
-    pygame.init()
-    pygame.display.set_mode((800, 600), flags=pygame.OPENGL | pygame.DOUBLEBUF, vsync=False)
-
     MAXIMUM_FPS = 60
-
     paused = False
     time = 0
     delta_time = 0
 
     def __init__(self):
+        pygame.init()
+        pygame.display.set_mode((800, 600), flags=pygame.OPENGL | pygame.DOUBLEBUF, vsync=False)
+        pygame.display_set_caption("Damien's Doom")
+        pygame.event.set_grab(True)
+        pygame.mouse.set_visible(True)
+
         if moderngl.get_context() == None:
             self.ctx = moderngl.create_context()
         else:
             self.ctx = moderngl.get_context()
 
-        pygame.event.set_grab(True)
-        pygame.mouse.set_visible(True)
+        self.ctx.enable(mgl.DEPTH_TEST | mgl.CULL_FACE)
+        self.clock = pygame.time.Clock()
 
         self.program = self.ctx.program(
             vertex_shader=utils.load_file_contents("engine/shaders/default.vert"),
@@ -53,6 +55,8 @@ class Engine:
 
     def run(self):
         while True:
+            dt = self.clock.tick(60) / 1000.0
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
